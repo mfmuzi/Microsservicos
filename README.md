9c902e6ebfa3ff32ff85b868815463a930e541dd
mfmuzi <mfmuzi@gmail.com>
2021-10-02 10:58:15 -0300

    README

---
+++ README.md
@@ -0,0 +1,6 @@
+## Microsserviços em Spring Cloud com Java
+
+Construindo um ecossistema com arquitetura baseada em microsserviços usando Spring Cloud durante o Bootcamp da Capgemini.
+
+
+
---
+++ product-catalog/src/main/java/com/dio/productcatalog/model/Product.java
@@ -0,0 +1,37 @@
+package com.dio.productcatalog.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = "product", type = "catalog")
+public class Product {
+
+    @Id
+    private long id;
+    private String name;
+    private Integer amount;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
